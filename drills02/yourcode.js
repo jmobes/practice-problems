9fd39381d7f6d80402f639ba9ecea636bfc33b25
Joseph Moberly <jmobes15@gmail.com>
2019-08-26 16:56:19 -0700

    Finished all problems except last one

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,17 +1,41 @@
 
 
-function countOccurences(){
-
+function countOccurences(wordArray, targetWord){
+  var count = 0;
+  for(var i = 0; i < wordArray.length; i++) {
+    if(wordArray[i] === targetWord) {
+      count++;
+    }
+  }
+  return count;
 }
 
-function wordLengths(){
-
+function wordLengths(wordArray){
+  var wordLenArray = [];
+  for(var i = 0; i < wordArray.length; i++) {
+    wordLenArray.push(wordArray[i].length);
+  }
+  return wordLenArray;
 }
 
-function getMinMaxMean(){
-
+function getMinMaxMean(numArray){
+  var minVal = numArray[0];
+  var maxVal = numArray[0];
+  var sum = 0;
+  var average = 0;
+  for(var i = 0; i < numArray.length; i++) {
+    sum += numArray[i];
+    if(numArray[i] < minVal) {
+      minVal = numArray[i];
+    }
+    if(numArray[i] > maxVal) {
+      maxVal = numArray[i];
+    }
+  }
+  average = sum / numArray.length;
+  return {min:minVal, max:maxVal, mean:average};
 }
 
-function findMode(){
-
+function findMode(numArr){
+  for(var inne)
 }
