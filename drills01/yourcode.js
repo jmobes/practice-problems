234863ffc6de231291f974762f9a5132e80085f2
Joseph Moberly <jmobes15@gmail.com>
2020-04-22 18:00:23 -0700

    completed drill

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,16 +1,32 @@
 
-function sumArray(){
-	
+function sumArray(numArr){
+  let sum = 0;
+	for(let i = 0; i < numArr.length; i++) {
+    sum += numArr[i];
+  }
+  return sum;
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(numArr, target){
+  let sum = 0;
+  let count = 0;
+  for(let i = 0; i < numArr.length; i++) {
+    if(sum + numArr[i] > target) {
+      numArr = numArr.splice(i,1);
+    }
+    else {
+      sum += numArr[i];
+    }
+  }
+  return numArr;
 }
 
-function getAllNamesShorterThan(){
-
+function getAllNamesShorterThan(nameArr, nameTargetLength){
+  return nameArr.filter(name=> name.length < nameTargetLength);
 }
 
-function makeLabel(){
-
+function makeLabel(label){
+  return `${label[0].greeting} ${label[0].givenName}
+  ${label[0]["home address"].streetNumber}
+  ${label[0]["home address"].city}, ${label[0]["home address"].state} ${label[0]["home address"].zip}`;
 }
